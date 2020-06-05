0f5c8035f790b0e7834015ea956803a0c7026109
Shreyash Chandak <shreyashakl@gmail.com>
2020-06-05 23:06:40 +0530

    05-06-20 23:06

--- certification.js
+++ certification.js
@@ -6,4 +6,12 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
+   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "15px";
+  } else {
+    document.getElementById("header").style.fontSize = "30px";
+  }
 }
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
